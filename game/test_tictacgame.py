7d9bef35ddccd2921e76000471649c8aff7cb9fa
Anton Churkin <praetoriaixleg@gmail.com>
2021-11-09 22:13:31 +0300

    [*] Поправил тесты, не дописал комбинации ничьиных партий

--- game/test_tictacgame.py
+++ game/test_tictacgame.py
@@ -13,11 +13,41 @@ class TestTicTacGame(unittest.TestCase):
         answer = self.game.validation("1")
         self.assertTrue(answer)
 
+        answer = self.game.validation("2")
+        self.assertTrue(answer)
+        
+        answer = self.game.validation("3")
+        self.assertTrue(answer)
+        
+        answer = self.game.validation("4")
+        self.assertTrue(answer)
+
+        answer = self.game.validation("5")
+        self.assertTrue(answer)
+
+        answer = self.game.validation("6")
+        self.assertTrue(answer)
+        
+        answer = self.game.validation("7")
+        self.assertTrue(answer)
+
+        answer = self.game.validation("8")
+        self.assertTrue(answer)
+
+        answer = self.game.validation("9")
+        self.assertTrue(answer)
+
     def test_validation_negative(self):
         """Проверка неверной валидации"""
-
+        
         with self.assertRaises(ValidatedError):
             self.game.validation("r")
+
+        with self.assertRaises(ValidatedError):
+            self.game.validation("0")
+
+        with self.assertRaises(ValidatedError):
+            self.game.validation("10")
 
         with self.assertRaises(ValidatedError):
             self.game.validation("12")
@@ -27,6 +57,12 @@ class TestTicTacGame(unittest.TestCase):
 
         with self.assertRaises(ValidatedError):
             self.game.validation(" ")
+
+        with self.assertRaises(ValidatedError):
+            self.game.validation("-")
+
+        with self.assertRaises(ValidatedError):
+            self.game.validation("$")
 
     def test_hasnot_owner_positive(self):
         answer = self.game.hasnot_owner(1)
@@ -38,9 +74,118 @@ class TestTicTacGame(unittest.TestCase):
         with self.assertRaises(HasOwnerError):
             self.game.hasnot_owner(1)
 
-    def test_check_win(self):
+    def test_check_win_first_for_x(self):
         """тест выигрышных комбинаций"""
         self.game.board[0] = "X"
         self.game.board[1] = "X"
         self.game.board[2] = "X"
         self.assertTrue(self.game.check_win(self.game.board))
+        
+    def test_check_win_second_for_x(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[3] = "X"
+        self.game.board[4] = "X"
+        self.game.board[5] = "X"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_third_for_x(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[6] = "X"
+        self.game.board[7] = "X"
+        self.game.board[8] = "X"
+        self.assertTrue(self.game.check_win(self.game.board))
+        
+    def test_check_win_fourth_for_x(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[0] = "X"
+        self.game.board[4] = "X"
+        self.game.board[8] = "X"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_fifth_for_x(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[2] = "X"
+        self.game.board[4] = "X"
+        self.game.board[6] = "X"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_sixth_for_x(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[0] = "X"
+        self.game.board[3] = "X"
+        self.game.board[6] = "X"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_seventh_for_x(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[1] = "X"
+        self.game.board[4] = "X"
+        self.game.board[7] = "X"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_eighth_for_x(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[2] = "X"
+        self.game.board[5] = "X"
+        self.game.board[8] = "X"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_first_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[0] = "Y"
+        self.game.board[1] = "Y"
+        self.game.board[2] = "Y"
+        self.assertTrue(self.game.check_win(self.game.board))
+        
+    def test_check_win_second_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[3] = "O"
+        self.game.board[4] = "O"
+        self.game.board[5] = "O"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_third_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[6] = "O"
+        self.game.board[7] = "O"
+        self.game.board[8] = "O"
+        self.assertTrue(self.game.check_win(self.game.board))
+        
+    def test_check_win_fourth_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[0] = "O"
+        self.game.board[4] = "O"
+        self.game.board[8] = "O"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_fifth_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[2] = "O"
+        self.game.board[4] = "O"
+        self.game.board[6] = "O"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_sixth_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[0] = "O"
+        self.game.board[3] = "O"
+        self.game.board[6] = "O"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_seventh_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[1] = "O"
+        self.game.board[4] = "O"
+        self.game.board[7] = "O"
+        self.assertTrue(self.game.check_win(self.game.board))
+
+    def test_check_win_eighth_for_o(self):
+        """тест выигрышных комбинаций"""
+        self.game.board[2] = "O"
+        self.game.board[5] = "O"
+        self.game.board[8] = "O"
+        self.assertTrue(self.game.check_win(self.game.board))
+    
+    #def test_check_draw_first(self):
+
+    #def test_check_draw_second(self):
