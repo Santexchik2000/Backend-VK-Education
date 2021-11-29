db86e60b9ddddd9f461c4153b42edf7ffb1338fa
Anton Churkin <praetoriaixleg@gmail.com>
2021-11-29 03:45:43 +0300

    [+]Развернул django приложение, добавил функции заглушки проверил post

--- postdelivery/api/views.py
+++ postdelivery/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.http import require_GET, require_POST
-
-
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
 @require_POST  # делаем views доступным только от этого метода
 def create_contract(request):
     name = request.POST["name"]
